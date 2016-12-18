d5bdc3bb6c07e07c413c8aefa8a75f4c864ace89
Rakesh Reddy Mudireddy <rakeshreddymudireddy@rakeshs-macbook-pro.local>
2016-12-17 21:30:27 -0800

    third commit

--- RakeshDemoApp/ViewController.m
+++ RakeshDemoApp/ViewController.m
@@ -15,6 +15,8 @@
 @property(nonatomic,strong) UITextField * username;
 @property(nonatomic,strong) UITextField * password;
 @property(nonatomic,strong) UISwitch * swt;
+@property(nonatomic,strong) UISlider * slider;
+@property(nonatomic,strong) UIImageView * image;
 
 @end
 
@@ -39,6 +41,13 @@
     NSLog(@"%i",z);
     
     
+    _slider = [[UISlider alloc]init];
+    _slider.frame = CGRectMake(10, 300,self.view.frame.size.width-20, 30);
+    [_slider addTarget:self action:@selector(slidervaluechanged:) forControlEvents:UIControlEventValueChanged];
+    _slider.minimumValue = 0.0;
+    _slider.maximumValue = 1.0;
+    [self.view addSubview:_slider];
+    
     _username = [[UITextField alloc]init];
     _username.frame = CGRectMake(100, 160, 100, 30);
     _username.layer.borderColor = [UIColor redColor].CGColor;
@@ -55,9 +64,41 @@
     _password.secureTextEntry = YES;
     [self.view addSubview:_password];
     
+    _image = [[UIImageView alloc]initWithFrame:CGRectMake(10, 380, self.view.frame.size.width-20, 150)];
+    _image.image = [UIImage imageNamed:@"bike.jpg"];
+    [self.view addSubview:_image];
+    
+    NSArray * array = [[NSArray alloc]initWithObjects:@"Rakesh",@"Satish",@"Santosh", nil];
+//    NSLog(@"Array is %@", array);
+    UISegmentedControl * segment = [[UISegmentedControl alloc]initWithItems:array];
+    segment.frame = CGRectMake(10, 350, self.view.frame.size.width-20, 30);
+    [segment addTarget:self action:@selector(segvaluechanged:) forControlEvents:UIControlEventValueChanged];
+    
+    [self.view addSubview:segment];
+    
+    
+    NSDictionary * dict = [[NSDictionary alloc]initWithObjectsAndKeys:_username,@"username",_password,@"password",nil];
+    NSLog(@"Dictionary is %@",[dict objectForKey:@"password"]);
 //    _swt = [[UISwitch alloc]initWithFrame:CGRectMake(240, 60, 60, 40)];
 //    [_swt addTarget:self action:@selector(switchMethod) forControlEvents:UIControlEventValueChanged];
 //    [self.view addSubview:_swt];
+    
+}
+-(void)segvaluechanged:(UISegmentedControl *)seg;
+{
+    if(seg.selectedSegmentIndex == 0){
+        _image.image = [UIImage imageNamed:@"car.jpg"];
+    }
+    if(seg.selectedSegmentIndex == 1){
+        _image.image = [UIImage imageNamed:@"bike.jpg"];
+    }
+    if(seg.selectedSegmentIndex == 2){
+        _image.image = [UIImage imageNamed:@"van.jpg"];
+    }
+}
+-(void)slidervaluechanged:(UISlider *)sli {
+    self.view.backgroundColor = [UIColor colorWithRed:168.0/255.0 green:221.0/255.0 blue:61.0/255.0 alpha:1.0];
+    
     
 }
 
@@ -67,7 +108,6 @@
     }else {
     self.view.backgroundColor = [UIColor orangeColor];
     }
-    
 }
 
 -(void) textMethod {
